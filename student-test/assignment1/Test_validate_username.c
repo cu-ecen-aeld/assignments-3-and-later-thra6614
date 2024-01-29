70f7fbc26f1c5d72d96a604941b48165d925f444
thra6614 <thra6614@Ubuntu.myguest.virtualbox.org>
2024-01-28 22:59:10 -0700

    made adjustments for last step of assignment 2 and fixed malloc in Test_validate_username

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,8 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(my_username(),malloc_username_from_conf_file());
+	const char * username = my_username();
+	const char * malloced_username = malloc_username_from_conf_file();
+	TEST_ASSERT_TRUE_MESSAGE(username,malloced_username, "Username Verification Test Fail");
+	free(malloced_username);
 }
